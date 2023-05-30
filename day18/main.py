499c9b17af622c87fcdca50485b0b8cd3a9c3834
Jared <jared_danell@yahoo.com>
2023-05-30 10:51:31 -0600

    clean up code

--- day18/main.py
+++ day18/main.py
@@ -1,9 +1,12 @@
 import turtle
 import random
+
 r = lambda: random.randint(0, 255)
 
-def random_HEX_color():
+
+def random_hex_color():
     return '#%02X%02X%02X' % (r(), r(), r())
+
 
 def draw_square(ninja_turtle: turtle.Turtle):
     for _ in range(0, 4):
@@ -12,7 +15,7 @@ def draw_square(ninja_turtle: turtle.Turtle):
 
 
 def draw_dashed_line(ninja_turtle: turtle.Turtle):
-    for _ in range(0,15):
+    for _ in range(0, 15):
         ninja_turtle.forward(10)
         ninja_turtle.penup()
         ninja_turtle.forward(10)
@@ -21,23 +24,21 @@ def draw_dashed_line(ninja_turtle: turtle.Turtle):
 
 def draw_shapes(ninja_turtle: turtle.Turtle):
     for i in range(3, 11):
-        ninja_turtle.pencolor(random_HEX_color())
+        ninja_turtle.pencolor(random_hex_color())
         for _ in range(i):
             ninja_turtle.fd(100)
-            ninja_turtle.right(360/i)
+            ninja_turtle.right(360 / i)
 
 
 tim = turtle.Turtle()
 tim.shape("turtle")
-tim.color(random_HEX_color())
+tim.color(random_hex_color())
 draw_square(tim)
 tim.reset()
-tim.color(random_HEX_color())
+tim.color(random_hex_color())
 draw_dashed_line(tim)
 tim.reset()
 draw_shapes(tim)
-
-
 
 screen = turtle.Screen()
 screen.exitonclick()
