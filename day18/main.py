6626d840615c6ad8cb9629abf0160c3963a69534
Jared <jared_danell@yahoo.com>
2023-05-30 11:37:28 -0600

    clean up code

--- day18/main.py
+++ day18/main.py
@@ -1,28 +1,29 @@
 import turtle
 import random
 
-r = lambda: random.randint(0, 255)
-
 
 def random_hex_color():
-    return '#%02X%02X%02X' % (r(), r(), r())
+    return '#' + ''.join(random.choices('0123456789ABCDEF', k=6))
 
 
-def draw_square(ninja_turtle: turtle.Turtle):
-    for _ in range(0, 4):
+def draw_square(ninja_turtle):
+    ninja_turtle.pensize(1)
+    for _ in range(4):
         ninja_turtle.forward(100)
         ninja_turtle.right(90)
 
 
-def draw_dashed_line(ninja_turtle: turtle.Turtle):
-    for _ in range(0, 15):
+def draw_dashed_line(ninja_turtle):
+    ninja_turtle.pensize(1)
+    for _ in range(15):
         ninja_turtle.forward(10)
         ninja_turtle.penup()
         ninja_turtle.forward(10)
         ninja_turtle.pendown()
 
 
-def draw_shapes(ninja_turtle: turtle.Turtle):
+def draw_shapes(ninja_turtle):
+    ninja_turtle.pensize(1)
     for i in range(3, 11):
         ninja_turtle.pencolor(random_hex_color())
         for _ in range(i):
@@ -30,11 +31,11 @@ def draw_shapes(ninja_turtle: turtle.Turtle):
             ninja_turtle.right(360 / i)
 
 
-def draw_random_walk(ninja_turtle: turtle.Turtle):
+def draw_random_walk(ninja_turtle):
     ninja_turtle.pensize(15)
     ninja_turtle.speed(10)
     directions = [0, 90, 180, 270]
-    for _ in range(0, 200):
+    for _ in range(200):
         ninja_turtle.pencolor(random_hex_color())
         direction = random.choice(directions)
         ninja_turtle.setheading(direction)
@@ -43,16 +44,17 @@ def draw_random_walk(ninja_turtle: turtle.Turtle):
 
 
 tim = turtle.Turtle()
-tim.shape("turtle")
-tim.color(random_hex_color())
+tim.shape('turtle')
+
 draw_square(tim)
 tim.reset()
-tim.color(random_hex_color())
+
 draw_dashed_line(tim)
 tim.reset()
+
 draw_shapes(tim)
 tim.reset()
+
 draw_random_walk(tim)
 
-screen = turtle.Screen()
-screen.exitonclick()
+turtle.Screen().exitonclick()
