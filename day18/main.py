2beade916d9c1f30ea3fab191113f4a1f6112457
Jared <jared_danell@yahoo.com>
2023-05-30 10:31:51 -0600

    drawing a shape within a shape with in a shape with random colors

--- day18/main.py
+++ day18/main.py
@@ -1,5 +1,6 @@
 import turtle
-
+import random
+r = lambda: random.randint(0, 255)
 
 def draw_square(ninja_turtle: turtle.Turtle):
     for _ in range(0, 4):
@@ -9,7 +10,7 @@ def draw_square(ninja_turtle: turtle.Turtle):
 
 def draw_dashed_line(ninja_turtle: turtle.Turtle):
     ninja_turtle.up()
-    ninja_turtle.setpos(-500, 300)
+    ninja_turtle.setpos(-200, 100)
     ninja_turtle.down()
     for _ in range(0,15):
         ninja_turtle.forward(10)
@@ -17,11 +18,27 @@ def draw_dashed_line(ninja_turtle: turtle.Turtle):
         ninja_turtle.forward(10)
         ninja_turtle.pendown()
 
+
+def draw_shapes(ninja_turtle: turtle.Turtle):
+    ninja_turtle.up()
+    ninja_turtle.up()
+    ninja_turtle.setpos(200, -100)
+    ninja_turtle.down()
+    for i in range(3, 11):
+        ninja_turtle.pencolor('#%02X%02X%02X' % (r(), r(), r()))
+        for _ in range(i):
+            ninja_turtle.fd(100)
+            ninja_turtle.right(360/i)
+
+
 tim = turtle.Turtle()
 tim.shape("turtle")
 tim.color("red")
 draw_square(tim)
 draw_dashed_line(tim)
+draw_shapes(tim)
 
 screen = turtle.Screen()
+
+print(screen.screensize())
 screen.exitonclick()
