62c65999efaee93b371fdfd84fbfdc069683cf4d
Jared <jared_danell@yahoo.com>
2023-07-13 11:49:19 -0600

    day 29 password manager saves passwords in plane txt but it does autogen passwords.

--- days26-50/day29/main.py
+++ days26-50/day29/main.py
@@ -1,16 +1,52 @@
+import random
+import string
+import pyperclip
 from tkinter import *
+from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    length = random.randint(8, 16)
+    password_entry.delete(0, END)
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Ensure at least one of each required character type
+    password = [random.choice(string.ascii_uppercase), random.choice(string.ascii_lowercase),
+                random.choice(string.digits), random.choice(string.punctuation)]
+
+    # Fill the remaining password length with random characters
+    password.extend(random.choice(characters) for _ in range(length))
+
+    # Shuffle the password to ensure randomness
+    random.shuffle(password)
+
+    password_str =  ''.join(str(char) for char in password)
+    password_entry.insert(END, password_str)
+    pyperclip.copy(password_str)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    with open("data.txt", mode='a') as data_file:
-        data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
-    website_entry.delete(0, END)
-    email_entry.delete(0, END)
-    password_entry.delete(0, END)
-    email_entry.insert(END, "matruim")
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if website == "" or email == "" or password == "":
+        messagebox.showinfo(title="Oops", message="Please dont leave any fields empty!")
+        return
+
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                          f"\nPassword: {password} \nIs it ok to save?")
+    if is_ok:
+        with open("data.txt", mode='a') as data_file:
+            data_file.write(f"{website} | {email} | {password}\n")
+
+        website_entry.delete(0, END)
+        email_entry.delete(0, END)
+        password_entry.delete(0, END)
+        email_entry.insert(END, "matruim")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Window setup
@@ -43,7 +79,7 @@ website_entry.focus()
 email_entry.insert(END, "matruim")
 
 # Buttons setup
-gen_pass_btn = Button(text="Generate Password")
+gen_pass_btn = Button(text="Generate Password", command=generate_password)
 add_btn = Button(text="Add", width=36, command=save_password)
 gen_pass_btn.grid(column=2, row=3)
 add_btn.grid(column=1, row=4, columnspan=2)
