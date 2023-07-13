0596d585510b745b835e4e26475b0b2948610665
Jared <jared_danell@yahoo.com>
2023-07-13 10:11:01 -0600

    pomodoro app done. could use some modification. would be nice to add sound and force to top of screen.

--- days26-50/day28/pomodoro-start/main.py
+++ days26-50/day28/pomodoro-start/main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,11 +10,82 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECK = "✓"
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    title.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    check_marks.config(text="")
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+
+    if reps % 8 == 0:
+        reps = 0
+        title.config(text="Break", fg=RED)
+        count_down(LONG_BREAK_MIN * 60)
+    elif reps % 2 == 0:
+        title.config(text="Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60)
+    else:
+        title.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global reps
+    count_min = math.floor(count / 60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    count_display = f"{count_min}:{count_sec}"
+
+    canvas.itemconfig(timer_text, text=count_display)
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        for _ in range(math.floor(reps / 2)):
+            marks += CHECK
+            check_marks.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50, "normal"), bg=YELLOW)
+title.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_image)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start = Button(text="Start", highlightthickness=0, command=start_timer)
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset.grid(column=2, row=2)
+
+check_marks = Label(font=(FONT_NAME, 18, "normal"), bg=YELLOW, fg=GREEN)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
