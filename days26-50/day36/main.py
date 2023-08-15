08cf2d05c28926f92dd169feb0ab307f0c52ccb5
Jared <jared_danell@yahoo.com>
2023-08-15 10:23:09 -0600

    Code clean up and optimization

--- days26-50/day36/main.py
+++ days26-50/day36/main.py
@@ -1,64 +1,64 @@
 import datetime
 import requests
-from dotenv import load_dotenv
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
 
 load_dotenv()
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+TWILIO_AUTH_TOKEN = os.getenv("TWILLIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILLIO_PHONE_NUMBER")
+MY_PHONE_NUMBER = os.getenv("MY_PHONE_NUMBER")
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
 def get_stock_price():
-    res = requests.get(STOCK_ENDPOINT, params={
+    params = {
         "function": "TIME_SERIES_DAILY",
         "symbol": STOCK_NAME,
         "outputsize": "compact",
         "datatype": "json",
-        "apikey": os.getenv("ALPHAVANTAGE_API_KEY")
-    })
+        "apikey": ALPHAVANTAGE_API_KEY
+    }
+    res = requests.get(STOCK_ENDPOINT, params=params)
     res.raise_for_status()
-    first2pairs = [res.json()["Time Series (Daily)"][k] for k in sorted(res.json()["Time Series (Daily)"].keys())[:2]]
-    yesterday = first2pairs[0]
-    day_before_yesterday = first2pairs[1]
-    print(yesterday)
-    print(day_before_yesterday)
-    delta = abs(float(yesterday["4. close"]) - float(day_before_yesterday["4. close"]))
-    print(delta)
-    percent = (delta / max(float(yesterday["4. close"]), float(day_before_yesterday["4. close"]))) * 100
-    print("{:.2f}".format(percent))
+    time_series = res.json()["Time Series (Daily)"]
+    recent_days = list(time_series.keys())[:2]
+    (yesterday_close, day_before_yesterday_close) = [float(time_series[day]["4. close"]) for day in recent_days]
+    delta = abs(yesterday_close - day_before_yesterday_close)
+    percent = (delta / max(yesterday_close, day_before_yesterday_close)) * 100
+    symbol = "🔺" if yesterday_close > day_before_yesterday_close else "🔻"
+
     if percent >= 5:
-        symbol = "🔺" if float(yesterday["4. close"]) > float(day_before_yesterday["4. close"]) else "🔻"
         get_news(symbol, "{:.2f}".format(percent))
 
 
 def get_news(symbol, percent):
-    res = requests.get(NEWS_ENDPOINT, params={
-        "apiKey": os.getenv("NEWS_API_KEY"),
-        "q":STOCK_NAME,
-        "from": datetime.datetime.today() - datetime.timedelta(days=3)
-    })
+    params = {
+        "apiKey": NEWS_API_KEY,
+        "q": STOCK_NAME,
+        "from": (datetime.datetime.today() - datetime.timedelta(days=3)).strftime('%Y-%m-%d')
+    }
+    res = requests.get(NEWS_ENDPOINT, params=params)
     res.raise_for_status()
-    first_3_articles = res.json()["articles"][:3]
-    print(first_3_articles)
-    for article in first_3_articles:
+    articles = res.json()["articles"][:3]
+    for article in articles:
         message = f"{STOCK_NAME}: {symbol}{percent}%\nHeadline: {article['title']}\nBrief: {article['description']}"
-        print(message)
+        send_sms(message)
 
 
 def send_sms(message_text):
-    account_sid = "AC6b5d19d6a2400792fbb01b4fa5afd74b"
-    auth_token = os.getenv("TWILLIO_AUTH_TOKEN")
-    client = Client(account_sid, auth_token)
-
+    client = Client(TWILIO_AUTH_TOKEN, TWILIO_AUTH_TOKEN)
     message = client.messages.create(
-        from_=os.getenv("TWILLIO_PHONE_NUMBER"),
+        from_=TWILIO_PHONE_NUMBER,
         body=message_text,
-        to=os.getenv("MY_PHONE_NUMBER")
+        to=MY_PHONE_NUMBER
     )
     print(message.status)
 
