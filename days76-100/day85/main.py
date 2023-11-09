b571c78610eaaa8448fa078827ad855823c956f8
Jared <jared_danell@yahoo.com>
2023-11-09 10:19:42 -0700

    code clean up

--- days76-100/day85/main.py
+++ days76-100/day85/main.py
@@ -3,20 +3,14 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-
 file_path = ""
-watermark = Image.open('cyberpunk.png')
+watermark = Image.open('cyberpunk.png').convert('RGBA')
 
 
 def adjust(image, size):
-    image = image.convert('RGBA')
-    width, height = image.size
-    new_width = size[0]
-    new_height = new_width * height // width
-    image = image.resize((new_width, new_height), resample=Image.Resampling.LANCZOS)
+    image = image.resize((size[0], size[0] * image.size[1] // image.size[0]), resample=Image.Resampling.LANCZOS)
     new_image = Image.new('RGBA', size, (0, 0, 0, 0))
-    upper = (size[1] - image.size[1]) // 2
-    new_image.paste(image, (0, upper))
+    new_image.paste(image, (0, (size[1] - image.size[1]) // 2))
     new_image.putalpha(50)
     return new_image
 
@@ -28,32 +22,29 @@ def open_image():
     if file_path:
         display_image()
 
-def get_watermarkedImage():
-    global watermark
+
+def get_watermarked_image():
     image_raw = Image.open(file_path).convert('RGBA')
-    watermark = adjust(watermark, image_raw.size)
-    return Image.alpha_composite(image_raw, watermark)
+    return Image.alpha_composite(image_raw, adjust(watermark, image_raw.size))
 
 
 def save_image():
-    fullpath = os.path.dirname(file_path)
-    print(f'{fullpath}/watermarked.png')
-    image = get_watermarkedImage()
-    image.save(f'{fullpath}/watermarked.png')
+    full_path = os.path.dirname(file_path)
+    get_watermarked_image().save(f'{full_path}/watermarked.png')
 
 
 def display_image():
-    photo = ImageTk.PhotoImage(get_watermarkedImage())
+    photo = ImageTk.PhotoImage(get_watermarked_image())
     image_label.config(image=photo)
     image_label.photo = photo
 
 
 root = tk.Tk()
 root.title("Simple Image Viewer")
-open_button = tk.Button(root, text="Open Image", command=open_image)
-open_button.pack(padx=20, pady=10)
+
+tk.Button(root, text="Open Image", command=open_image).pack(padx=20, pady=10)
 image_label = tk.Label(root)
 image_label.pack(padx=20, pady=20)
-save_button = tk.Button(root, text="Save Image", command=save_image)
-save_button.pack(padx=20, pady=10)
+tk.Button(root, text="Save Image", command=save_image).pack(padx=20, pady=10)
+
 root.mainloop()
